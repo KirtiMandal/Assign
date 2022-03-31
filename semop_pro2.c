9af1a420d608ee681bd2b7027f47be81bff3dc84
KirtiMandal <98145950+KirtiMandal@users.noreply.github.com>
2022-03-31 11:01:13 +0000

    Add files via upload

---
+++ semop_pro2.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+union semnum{
+		int val;
+		struct sid_ds *buf;
+		unsigned short *array;
+		struct seminfo *__buf;
+	};
+	
+int released_semaphore(void);
+int get_semaphore(void);
+struct sembuf sem_op;
+union semnum sem_union;
+int sem_id;
+
+int main(){
+	int i,j;
+	sem_id=semget((key_t)1234,1,0666|IPC_CREAT);
+	
+	for(i=0;i<=10;i++){
+		get_semaphore();
+		printf("\n%d : Got the semaforce\n",getpid());
+		for(j=0;j<=3;j++){
+		sleep(1);
+		printf("\a");
+	 }
+		printf("\nSEM: %d released the semaphore\n",getpid());
+		release_semaphore();
+		sleep(2);
+		
+	}
+
+	sem_union.val=1;
+	
+	if(semctl(sem_id,0,SETVAL,sem_union)<0)
+		printf("Fails to delete the semaphore\n");
+		
+	else 
+		printf("Semaphore deleted\n");
+}
+
+int get_semaphore(void){
+		sem_op.sem_num=0;
+		sem_op.sem_op=-1;
+		sem_op.sem_flg=SEM_UNDO;
+		
+	if(semop(sem_id,&sem_op,1)<0){
+		printf("Failed\n");
+		return -1;
+		}
+}
+
+int release_semaphore(void){
+		sem_op.sem_num=0;
+		sem_op.sem_op=1;
+		sem_op.sem_flg=SEM_UNDO;
+	if(semop(sem_id,&sem_op,1)<0){
+		printf("Failed\n");
+		return -1;
+		}
+}
